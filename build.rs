5019d3478329f478b85203f3e971a1350e2f1741
chrysn <chrysn@fsfe.org>
2018-10-01 14:21:57 +0200

    Use RIOT's CFLAGS
    Touching into pktsnips.type_ caused issues b/c the C modules were built
    with -fshort-enums and clang did not know that.
    
    Now, all CFLAGS are passed on, with some exceptions which GCC knows but
    clang doesn't, and without strict errors, as obviously there would be
    macro redefinitions.

--- build.rs
+++ build.rs
@@ -1,4 +1,5 @@
 extern crate bindgen;
+extern crate shlex;
 
 use bindgen::builder;
 use std::env;
@@ -8,11 +9,26 @@ fn main() {
     let sourcefile = env::var("RIOT_EXPANDED_HEADER")
         .expect("Please set RIOT_EXPANDED_HEADER, see README for details.");
 
+    let cflags = env::var("CFLAGS")
+        .expect("Please pass in the same CFLAGS that were used to build RIOT.");
+    let cflags = shlex::split(&cflags)
+        .expect("Odd shell escaping in CFLAGS");
+
     println!("cargo:rerun-if-env-changed=RIOT_EXPANDED_HEADER");
+    println!("cargo:rerun-if-env-changed=CFLAGS");
     println!("cargo:rerun-if-changed={}", sourcefile);
 
     let bindings = builder()
         .header(sourcefile)
+        .clang_args(cflags.iter().filter(|x| {
+            match x.as_ref() {
+                "-Werror" => false,
+                "-mno-thumb-interwork" => false,
+                "-Wformat-overflow" => false,
+                "-Wformat-truncation" => false,
+                _ => true,
+            }
+        }))
         .use_core()
         .ctypes_prefix("libc")
         .impl_debug(true)
