3b11ef63a39b91a213fbc2a33fd7d2af16d3e2a6
chrysn <chrysn@fsfe.org>
2018-10-04 19:10:41 +0200

    Take CFLAGS in dedicated variable
    Running cargo with foreign-platform CFLAGS makes them apply to other
    build processes inside cargo, which typically don't react well to them
    (but that can go unnoticed while nothing of the build infrastructure is
    rebuilt).

--- build.rs
+++ build.rs
@@ -9,13 +9,13 @@ fn main() {
     let sourcefile = env::var("RIOT_EXPANDED_HEADER")
         .expect("Please set RIOT_EXPANDED_HEADER, see README for details.");
 
-    let cflags = env::var("CFLAGS")
-        .expect("Please pass in the same CFLAGS that were used to build RIOT.");
+    let cflags = env::var("RIOT_CFLAGS")
+        .expect("Please pass in the same RIOT_CFLAGS like RIOT_EXPANDED_HEADER.");
     let cflags = shlex::split(&cflags)
-        .expect("Odd shell escaping in CFLAGS");
+        .expect("Odd shell escaping in RIOT_CFLAGS");
 
     println!("cargo:rerun-if-env-changed=RIOT_EXPANDED_HEADER");
-    println!("cargo:rerun-if-env-changed=CFLAGS");
+    println!("cargo:rerun-if-env-changed=RIOT_CFLAGS");
     println!("cargo:rerun-if-changed={}", sourcefile);
 
     let bindings = builder()
