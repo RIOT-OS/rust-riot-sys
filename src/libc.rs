5131ec5cd669d5b3a1e6a54de0fbf67c47a8f868
chrysn <chrysn@fsfe.org>
2018-12-11 20:53:54 +0100

    Update to 2018 edition
    with changes from `cargo fix --edition --edition-idioms`

--- src/libc.rs
+++ src/libc.rs
@@ -92,7 +92,7 @@ impl CStr {
 macro_rules! cstr {
     ($s:expr) => {{
         let a = concat!($s, "\0");
-        unsafe { ::libc::CStr::from_bytes_with_nul_unchecked(a.as_bytes()) }
+        unsafe { crate::libc::CStr::from_bytes_with_nul_unchecked(a.as_bytes()) }
     }};
 }
 
