8010fbbe05c67179feffdf3da466a3e172e394b5
chrysn <chrysn@fsfe.org>
2020-09-03 15:40:50 +0200

    build: Make CFLAGS usable for C2Rust

--- build.rs
+++ build.rs
@@ -14,26 +14,28 @@ fn main() {
     println!("cargo:rerun-if-env-changed=RIOT_CFLAGS");
     println!("cargo:rerun-if-changed=riot-all.h");
 
+    let cflags = cflags.iter().filter(|x| {
+        match x.as_ref() {
+            // non-clang flags showing up with arm cortex m3 (eg. stk3700 board)
+            "-Werror" => false,
+            "-mno-thumb-interwork" => false,
+            "-Wformat-overflow" => false,
+            "-Wformat-truncation" => false,
+            // non-clang flags showing up for the hifive1 board
+            "-mcmodel=medlow" => false,
+            "-msmall-data-limit=8" => false,
+            "-nostartfiles" => false, // that probably shows up on arm too, but shouldn't matter
+            "-fno-delete-null-pointer-checks" => false, // seen on an Ubuntu 18.04
+            // and much more worries on that ubuntu ... maybe just recommend TOOLCHAIN=llvm ?
+            // accept all others
+            _ => true,
+        }
+    });
+
     let bindings = builder()
         .header("riot-all.h")
         .size_t_is_usize(true)
-        .clang_args(cflags.iter().filter(|x| {
-            match x.as_ref() {
-                // non-clang flags showing up with arm cortex m3 (eg. stk3700 board)
-                "-Werror" => false,
-                "-mno-thumb-interwork" => false,
-                "-Wformat-overflow" => false,
-                "-Wformat-truncation" => false,
-                // non-clang flags showing up for the hifive1 board
-                "-mcmodel=medlow" => false,
-                "-msmall-data-limit=8" => false,
-                "-nostartfiles" => false, // that probably shows up on arm too, but shouldn't matter
-                "-fno-delete-null-pointer-checks" => false, // seen on an Ubuntu 18.04
-                // and much more worries on that ubuntu ... maybe just recommend TOOLCHAIN=llvm ?
-                // accept all others
-                _ => true,
-            }
-        }))
+        .clang_args(cflags)
         .use_core()
         .ctypes_prefix("libc")
         .impl_debug(true)
