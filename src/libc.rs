17311826a1ff70a27499f67839da71c8082625e4
chrysn <chrysn@fsfe.org>
2021-03-26 09:05:52 +0100

    Explain why size_t is not mapped

--- src/libc.rs
+++ src/libc.rs
@@ -8,6 +8,7 @@
 pub use cty::{
     c_char, c_double, c_float, c_int, c_long, c_longlong, c_schar, c_short, c_uchar, c_uint,
     c_ulong, c_ulonglong, c_ushort,
+    // Not even loading size_t and ssize_t as they don't fit with bindgen's mapping anyway
 };
 
 // Used to be a dedicated type, pub-used to avoid breaking the API
