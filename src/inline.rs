3657a64bb3761307aed3ff14d3f543721ef6ed98
chrysn <chrysn@fsfe.org>
2022-01-17 13:28:18 +0100

    Work around C library's used but undefined __locale_t type

--- src/inline.rs
+++ src/inline.rs
@@ -88,4 +88,9 @@ use cty as libc;
 
 use c2rust_bitfields::*;
 
+// This is a replacement for the `pub type __locale_t` that C2Rust generates because of something
+// from stdlib; it is stripped out of the compiled code and turned into a void pointer for lack of
+// better ideas. (Leaving it as a pub struct would require unstable Rust).
+pub type __locale_t = libc::c_void;
+
 include!(concat!(env!("OUT_DIR"), "/riot_c2rust_replaced.rs"));
