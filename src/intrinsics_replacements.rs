5a9cfc159cc73efe0f0cfecaa5169aaaf7886e27
chrysn <chrysn@fsfe.org>
2022-03-09 00:33:52 +0100

    Limit intrinsics workaround to RISC-V

--- src/intrinsics_replacements.rs
+++ src/intrinsics_replacements.rs
@@ -1,16 +1,18 @@
 //! Replacements for [core::intrinsics] to work around <https://github.com/immunant/c2rust/issues/372>
-// These were observed to be used on riscv with the hello-world example; it's hard to predict when
-// they are here, and allowing them to stay does no harm. (Given they are a workaround, they have
-// an expiry date anyway).
-#![allow(dead_code)]
+//!
+//! They are currently only define on riscv32; other platforms (or functions) can be added when
+//! necessary. They can't be defined for all platforms, as Cortex-M0 don't have this operation.
+//! (But then again, they're not needed, as theire RIOT C code can't contain these operations).
 
 use core::sync::atomic::{AtomicU32, Ordering};
 
+#[cfg(target_arch = "riscv32")]
 fn atomic_and_relaxed(dst: *mut u32, src: u32) -> u32 {
     let actual_atomic = unsafe { &*(dst as *mut AtomicU32) };
     actual_atomic.fetch_and(src, Ordering::Relaxed)
 }
 
+#[cfg(target_arch = "riscv32")]
 fn atomic_or_relaxed(dst: *mut u32, src: u32) -> u32 {
     let actual_atomic = unsafe { &*(dst as *mut AtomicU32) };
     actual_atomic.fetch_or(src, Ordering::Relaxed)
