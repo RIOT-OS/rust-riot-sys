506b97e09c99ae2f1266cc9bfc7a2aafbcfa8510
chrysn <chrysn@fsfe.org>
2022-03-08 09:58:10 +0100

    (semicolon fix)

--- src/inline.rs
+++ src/inline.rs
@@ -80,7 +80,7 @@ macro_rules! llvm_asm {
     };
     ("csrr $0, mstatus" : : "=r" ($state:ident) : "memory" : "volatile") => {
         core::arch::asm!("csrr {}, mstatus", out(reg) $state);
-    }
+    };
     ($($x:tt)*) => {{
         llvm_asm_is_not_supported_any_more();
         unreachable!()
